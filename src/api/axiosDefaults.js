46e27997cfaeb16f6ed77df9296d509380f23662
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-03-13 11:37:49 +0000

    created axios instances, one for the request and response

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://snapgram-api-df7c5b682dbd.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const  axiosReq = axios.create();
+export const axiosRes = axios.create();
