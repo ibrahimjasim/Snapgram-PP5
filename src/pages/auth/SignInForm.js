5873d582907e22728cc4017dad585a386bef1906
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-03-13 10:18:54 +0000

    Created the SignIn form

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -21,9 +21,20 @@ function SignInForm() {
     <Row className={styles.Row}>
       <Col className="my-auto p-0 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign in</h1>
-          {/* Add your form here */}
+        <Form>
+  <Form.Group controlId="username">
+    <Form.Label className="d-none">Username</Form.Label>
+    <Form.Control type="text" placeholder="Username" name="username"/>
+  </Form.Group>
 
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label className="d-none">Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" name="password"/>
+  </Form.Group>
+  <Button variant="primary" type="submit">
+  Sign In
+  </Button>
+</Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signup">
