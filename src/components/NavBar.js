710403bfa679eae57295a336bc36a130b41ba7ea
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-03-13 20:03:55 +0000

    added handleClickOutSide function

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
@@ -15,6 +15,19 @@ const NavBar = () => {
   const setCurrentUser = useSetCurrentUser();
 
   const [expanded, setExpanded] = useState(false)
+  const ref = useRef(null)
+  useEffect(()=>{
+    const handleClickOutSide =(event)=> {
+      if (ref.current && !ref.current.contains(event.target)){
+        setExpanded(false)
+      }
+    }
+
+    document.addEventListener('mouseup', handleClickOutSide)
+    return () => {
+      document.removeEventListener('mouseup', handleClickOutSide)
+    }
+  }, [ref])
 
   const handleSignOut = async () => {
     try {
@@ -89,7 +102,7 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
-        <Navbar.Toggle onClick={()=> setExpanded(!expanded)} aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle ref={ref} onClick={()=> setExpanded(!expanded)} aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
