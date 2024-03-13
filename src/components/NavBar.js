e1fa272740688350c55b5df8dc6780cf5b4c7175
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-03-13 19:27:25 +0000

    added loggedInIcon and added the addpost, feed, liked, signout Navlinks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,46 @@ import { useCurrentUser } from "../contexts/CurrentUserContext";
 const NavBar = () => {
   const currentUser = useCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const addPostIcon = (
+    <NavLink
+    className={styles.NavLink}
+    activeClassName={styles.Active}
+    to="/post/create"
+  >
+    <i className="far fa-plus-square"></i>Add post
+  </NavLink>
+  );
+
+  const loggedInIcons = (<>
+    <NavLink
+    className={styles.NavLink}
+    activeClassName={styles.Active}
+    to="/feed"
+  >
+    <i className="fas fa-stream"></i>Feed
+  </NavLink>
+  <NavLink
+    className={styles.NavLink}
+    activeClassName={styles.Active}
+    to="/feed"
+  >
+    <i className="fas fa-heart"></i>Liked
+  </NavLink>
+  <NavLink
+    className={styles.NavLink}
+    to="/"
+    onClick={()=> {}}
+  >
+    <i className="fas fa-sign-out-alt"></i>Sign out
+  </NavLink>
+  <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <img src={currentUser?.profile_image} />
+      </NavLink>
+  </>
+  );
   const loggedOutIcons = (
     <>
       <NavLink
@@ -36,7 +75,7 @@ const NavBar = () => {
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
-
+        {currentUser && addPostIcon}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
