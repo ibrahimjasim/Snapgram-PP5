d719b4d2e22e4442ab8389612f1efed5a98d2da6
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-03-13 20:12:03 +0000

    set the useEffect cleanup function to remove the event listener

---
+++ src/hooks/useClickOutsideToggle.js
@@ -0,0 +1,23 @@
+import { useEffect, useRef, useState } from 'react'
+
+const useClickOutsideToggle = () => {
+    const [expanded, setExpanded] = useState(false)
+    const ref = useRef(null)
+    useEffect(()=>{
+      const handleClickOutSide =(event)=> {
+        if (ref.current && !ref.current.contains(event.target)){
+          setExpanded(false)
+        }
+      }
+  
+      document.addEventListener('mouseup', handleClickOutSide)
+      return () => {
+        document.removeEventListener('mouseup', handleClickOutSide)
+      }
+    }, [ref])
+  return {
+    expanded, setExpanded, ref
+  };
+}
+
+export default useClickOutsideToggle
