decb95e438483b9626ee90c4c28f9806a21b0a00
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-03-15 15:54:55 +0000

    create currentFunction, added handleEdit, and handleDelete to MoreDropdown

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -3,9 +3,14 @@ import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import styles from "../../styles/Comment.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { MoreDropdown } from "../../components/MoreDropdown";
 
 const Comment = (props) => {
   const { profile_id, profile_image, owner, updated_at, content } = props;
+
+  const currentUser = useCurrentUser()
+  const is_owner = currentUser?.username ===owner;
 
   return (
     <div>
@@ -19,6 +24,9 @@ const Comment = (props) => {
           <span className={styles.Date}>{updated_at}</span>
           <p>{content}</p>
         </Media.Body>
+        {is_owner && (
+            <MoreDropdown handleEdit={() => {}} handleDelete={() => {}} />
+        )}
       </Media>
     </div>
   );
