4306f6c287963d7af4999d703f45be4b16bca557
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-04-09 08:38:48 +0000

    set up to create React unit tests,  wrote the API request handlers that need to mock responses from our API.

---
+++ src/moks/handlers.js
@@ -0,0 +1,21 @@
+import { rest } from "msw"
+
+const baseURL = "https://snapgram-api-df7c5b682dbd.herokuapp.com/"
+
+export const handlers = [
+    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+        return res(ctx.json({
+            "pk": 4,
+            "username": "Aya-ij",
+            "email": "",
+            "first_name": "",
+            "last_name": "",
+            "profile_id": 4,
+            "profile_image": "https://res.cloudinary.com/dju1fkr93/image/upload/v1/media/../default_profile_fqfn5s"
+        })
+        );
+    }),
+    rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+        return res(ctx.status(200));
+    }),
+];
